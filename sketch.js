dba6452782e06715b5a8efc8863ed50db4a16583
Aril-Lamay <72742540+Aril-Lamay@users.noreply.github.com>
2021-03-10 12:57:28 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,10 +1,16 @@
 var spaceShip, spaceShipImage;
-var asteroid, asteroidImage; 
+var asteroid, asteroidImage, asteroid1Image; 
 var enemySpaceShip, enemySpaceShipImage;
 var powerups;
 var bg, bgImage;
 var bullet, bulletImage;
 var laser,laserImage;
+var asteroidGroup;
+var gameState = "Start";
+var p1Score = 0;
+var p2Score = 0;
+var enemyLife = 5;
+var playerLife = 5;
 
 
 function preload(){
@@ -12,45 +18,108 @@ function preload(){
 
   spaceShipImage = loadImage('Images/spaceship.png');
   asteroidImage = loadImage('Images/asteroid.png');
-  enemySpaceShipImage = loadImage('Images/Daco_234062.png');
+  enemySpaceShipImage = loadImage('Images/spacecraftart.png');
   bulletImage = loadImage('Images/bullet.png');
   laserImage = loadImage('Images/pngegg.png');
+
+  asteroid1Image = loadImage('Images/asteroid1.png');
 }
 function setup() {
   createCanvas(1200, 600);
 
-  bg = createSprite(600,300,1200,600);
-  bg.addImage(bgImage);
 
   spaceShip = createSprite(150,300);
   spaceShip.addImage(spaceShipImage);
   spaceShip.scale = 0.17;
+  spaceShip.visible = false;
 
   enemySpaceShip = createSprite(1000,300);
   enemySpaceShip.addImage(enemySpaceShipImage);
-  enemySpaceShip.scale = 0.17;
- 
+  enemySpaceShip.scale = 0.37;
+  enemySpaceShip.visible = false;
+  
+  bullet = createSprite(500,spaceShip.y,30,5);
+  bullet.addImage(bulletImage);
+  bullet.scale = 0.5;
+  bullet.visible = false;
+
+  laser = createSprite(1000,enemySpaceShip.y,30,5);
+  laser.addImage(laserImage);
+  laser.scale = 0.2;
+  laser.visible = false;
+
+  asteroidGroup = createGroup();
+
 }
 
 function draw() {
   background(0, 0, 0);
-  spawnAsteroids();
-  movePlayer();
-  moveEnemy();
+  if(gameState === "Start"){
+    textSize(50);
+    fill("white");
+    text("PRESS 'ENTER' TO START THE GAME",200,200);
+    if(keyDown("enter")){
+      gameState = "Play";
+    }
+  }
+  else if(gameState === "Play"){
+
+    background(bgImage);
+    enemySpaceShip.visible = true;
+    spaceShip.visible = true;
+    spawnAsteroids();
+    movePlayer();
+    moveEnemy();
+
+    if(keyDown("space")){
+      bullet.x = spaceShip.x - 20;
+      bullet.y = spaceShip.y
+      bullet.velocityX = 15;
+      bullet.visible = true;
+    }
+    if(keyDown("shift")){
+      laser.x = enemySpaceShip.x - 20;
+      laser.y = enemySpaceShip.y
+      laser.velocityX = -15;
+      laser.visible = true;
+    }
+
+    if(asteroidGroup.isTouching(bullet) || asteroidGroup.isTouching(laser) ){
+      asteroidGroup.destroyEach();
+      bullet.visible = false;
+      laser.visible = false;
+      p1Score = p1Score - 1;
+      p2Score = p2Score - 1;
+    }
+
+    if(bullet.isTouching(enemySpaceShip)){
+      bullet.visible = false;
+      
+      p1Score = p1Score + 5;
+      enemyLife = enemyLife - 1;
+    }
+
+    textSize(25);
+    fill("white");
+    text("Player Score: " + p1Score,100,100);
+  }
   drawSprites();
 }
 
 function spawnAsteroids(){
-  if(frameCount%120 === 0){
+  if(frameCount%100 === 0){
     var randX = Math.round(random(1200,250));
     var randY = Math.round(random(100,-100));
 
     asteroid = createSprite(randX,randY,70,70);
     asteroid.addImage(asteroidImage);
     asteroid.scale = 0.1;
+
+    asteroid.setCollider("circle",100,100,100);
     if(asteroid.x < 600 && asteroid.y < 300){
       asteroid.velocityX = 5;
       asteroid.velocityY = 4; 
+      asteroid.addImage(asteroid1Image);
     }
     if(asteroid.x < 600 && asteroid.y > 300){
       asteroid.velocityX = 7;
@@ -60,14 +129,10 @@ function spawnAsteroids(){
     asteroid.velocityX = -6;
     asteroid.velocityY = 3;
     }
-  if(asteroid.x > 600 && asteroid.y > 300){
-    asteroid.velocityX = -4;
-    asteroid.velocityY = -7;
+  if(asteroid.x = 600 ){
+    asteroid.velocityY = 7;
     }
- 
-    //asteroid.velocityX = -4;
-    //asteroid.velocityY = 5;
-  console.log(asteroid.x,asteroid.y);
+  asteroidGroup.add(asteroid);
 }
 }
 
